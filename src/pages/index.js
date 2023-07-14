85a08b52bfa9d1b33f4386556f1b4fdf1233c90f
anikjoyy <anikjoy.cse@gmail.com>
2023-07-14 06:42:09 +0600

    created card component for movielist

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { MovieDetail } from './MovieDetail';
 export { MovieList } from './MovieList';
+export { MovieDetail } from './MovieDetail';
 export { Search } from './Search';
 export { PageNotFound } from './PageNotFound';
