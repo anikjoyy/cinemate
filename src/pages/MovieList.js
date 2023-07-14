b262b684c0c3df9b3fa9c4a62af85e4927e8ac98
anikjoyy <anikjoy.cse@gmail.com>
2023-07-14 07:40:29 +0600

    created custom hook to fetch movies from api

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,15 @@
-import { useEffect, useState } from 'react';
 import { Card } from '../components';
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
+import { useFetch } from '../hooks/useFetch';
 
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        'https://api.themoviedb.org/3/movie/now_playing?api_key=b80d59c33d6d57ed9c7e3713f91c188a'
-      );
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
 
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
   return (
     <main>
       <section className='max-w-7xl mx-auto py-7'>
         <div className='flex justify-start flex-wrap'>
-          {movies.map((movie, index) => (
-            <Card key={index} movie={movie} />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
           ))}
         </div>
       </section>
