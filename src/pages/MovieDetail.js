939c254ddc7f2220285bb32d044f9f886a484022
anikjoyy <anikjoy.cse@gmail.com>
2023-07-17 11:24:44 +0600

    solved some error

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -7,6 +7,7 @@ export const MovieDetail = () => {
   const params = useParams();
   const [movie, setMovie] = useState({});
 
+  //eslint-disable-next-line
   const pageTitle = useTitle(movie.title);
 
   const image = movie.poster_path
